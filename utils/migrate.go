c2769d16a05e68a8f8b828006f736e47110cace2
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-10-13 15:14:57 +0200

    Add migrate

---
+++ utils/migrate.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+const (
+	defaultMigrationHost     = "localhost"
+	defaultMigrationPort     = 5432
+	defaultMigrationUser     = "postgres"
+	defaultMigrationPassword = "password"
+	defaultMigrationDBName   = "postgres"
+)
+
+func MigrateBase() {
+	host := GetEnv("DB_HOST", defaultMigrationHost)
+	port := GetEnvAsInt("DB_PORT", defaultMigrationPort)
+	user := GetEnv("DB_USER", defaultMigrationUser)
+	password := GetEnv("DB_PASSWORD", defaultMigrationPassword)
+	dbname := GetEnv("DB_NAME", defaultMigrationDBName)
+
+	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		user, password, host, port, dbname)
+
+	m, err := migrate.New("file://migrations/base", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	if err := m.Up(); err != nil {
+		panic(err)
+	}
+}
