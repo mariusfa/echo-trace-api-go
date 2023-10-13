63636915962ee484839ec9f3a21ef02990e24fef
Marius Fagerland <marius.fagerland@sparebank1.no>
2023-10-13 15:33:46 +0200

    Impl user_repository.go

--- biz/user_repository.go
+++ biz/user_repository.go
@@ -1,6 +1,7 @@
 package biz
 
 import (
+	"database/sql"
 	"echo/biz/domain"
 )
 
@@ -15,3 +16,35 @@ type UserRepositoryContract interface {
 	Insert(user domain.User) error
 	GetByName(name string) (domain.User, error)
 }
+
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Insert(user domain.User) error {
+	query := "INSERT INTO echotraceschema.user (name, hashed_password, api_token) VALUES (?, ?, ?)"
+	_, err := r.db.Exec(query, user.Name, user.HashedPassword, user.ApiToken)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *UserRepository) GetByName(name string) (domain.User, error) {
+	query := "SELECT id, name, hashed_password, api_token FROM echotraceschema.user WHERE name = ?"
+	row := r.db.QueryRow(query, name)
+
+	var user domain.User
+	err := row.Scan(&user.Id, &user.Name, &user.HashedPassword, &user.ApiToken)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return user, nil
+}
+
