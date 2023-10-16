51cccf75b8d13d7f8026cb23416140ff731cb755
Marius Fagerland <mariusfagerland@yahoo.no>
2023-10-16 20:34:09 +0200

    Finish test

--- biz/user_repository_test.go
+++ biz/user_repository_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"database/sql"
 	"echo/biz"
 	"echo/biz/domain"
 	"echo/utils"
@@ -51,13 +52,14 @@ func TestUserRepository_Insert(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to setup app db: %v", err)
 	}
-	repo := biz.NewUserRepository(db)
 
 	user := domain.User{
 		Name:           "testuser",
 		HashedPassword: "testpass",
-		ApiToken:       "testtoken",
+		ApiToken:       "testtoken2",
 	}
+
+	repo := biz.NewUserRepository(db)
 	err = repo.Insert(user)
 	if err != nil {
 		t.Errorf("Failed to insert user: %v", err)
@@ -71,6 +73,17 @@ func TestUserRepository_Insert(t *testing.T) {
 		t.Errorf("Failed to get user: %v", err)
 	}
 
+	var count int
+	query = "SELECT COUNT(*) FROM echotraceschema.user"
+	err = db.QueryRow(query).Scan(&count)
+	if err != nil {
+		t.Errorf("Failed to get count: %v", err)
+	}
+
+	if count != 1 {
+		t.Errorf("Unexpected count: got %d, want %d", count, 1)
+	}
+
 	if result.Name != user.Name {
 		t.Errorf("Unexpected user name: got %s, want %s", result.Name, user.Name)
 	}
@@ -80,34 +93,65 @@ func TestUserRepository_Insert(t *testing.T) {
 	if result.ApiToken != user.ApiToken {
 		t.Errorf("Unexpected API token: got %s, want %s", result.ApiToken, user.ApiToken)
 	}
+
+	err = clearUserTable(db)
+	if err != nil {
+		t.Fatalf("Failed to clear user table: %v", err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		t.Fatalf("Failed to close db: %v", err)
+	}
 }
 
-// func TestUserRepository_GetByName(t *testing.T) {
-// 	repo := biz.NewUserRepository(db)
+func TestUserRepository_GetByName(t *testing.T) {
+	db, err := utils.SetupAppDb(userDbConfig)
+	if err != nil {
+		t.Fatalf("Failed to setup app db: %v", err)
+	}
+	repo := biz.NewUserRepository(db)
 
-// 	user := domain.User{
-// 		Name:           "testuser",
-// 		HashedPassword: "testpass",
-// 		ApiToken:       "testtoken",
-// 	}
-// 	err := repo.Insert(user)
-// 	if err != nil {
-// 		t.Errorf("Failed to insert user: %v", err)
-// 	}
+	// Insert a test user
+	user := domain.User{
+		Name:           "testuser",
+		HashedPassword: "testpass",
+		ApiToken:       "testtoken1",
+	}
+	err = repo.Insert(user)
+	if err != nil {
+		t.Fatalf("Failed to insert user: %v", err)
+	}
 
-// 	// Get the user by name
-// 	result, err := repo.GetByName(user.Name)
-// 	if err != nil {
-// 		t.Errorf("Failed to get user: %v", err)
-// 	}
+	// Get the user by name
+	result, err := repo.GetByName(user.Name)
+	if err != nil {
+		t.Errorf("Failed to get user: %v", err)
+	}
 
-// 	if result.Name != user.Name {
-// 		t.Errorf("Unexpected user name: got %s, want %s", result.Name, user.Name)
-// 	}
-// 	if result.HashedPassword != user.HashedPassword {
-// 		t.Errorf("Unexpected hashed password: got %s, want %s", result.HashedPassword, user.HashedPassword)
-// 	}
-// 	if result.ApiToken != user.ApiToken {
-// 		t.Errorf("Unexpected API token: got %s, want %s", result.ApiToken, user.ApiToken)
-// 	}
-// }
+	if result.Name != user.Name {
+		t.Errorf("Unexpected user name: got %s, want %s", result.Name, user.Name)
+	}
+	if result.HashedPassword != user.HashedPassword {
+		t.Errorf("Unexpected hashed password: got %s, want %s", result.HashedPassword, user.HashedPassword)
+	}
+	if result.ApiToken != user.ApiToken {
+		t.Errorf("Unexpected API token: got %s, want %s", result.ApiToken, user.ApiToken)
+	}
+
+	err = clearUserTable(db)
+	if err != nil {
+		t.Fatalf("Failed to clear user table: %v", err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		t.Fatalf("Failed to close db: %v", err)
+	}
+}
+
+func clearUserTable(db *sql.DB) error {
+	query := "DELETE FROM echotraceschema.user"
+	_, err := db.Exec(query)
+	return err
+}
